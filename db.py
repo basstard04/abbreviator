a4fc6d00223c738a2f51a05d1eb9bae451d5d189
basstard04 <kirill174_04@mail.ru>
2023-11-21 17:25:48 +0500

    git commit -m "4"

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-connect = sqlite3.connect('database.db', check_same_thread=False)
+connect = sqlite3.connect('bd.db', check_same_thread=False)
 cursor = connect.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS "users"
 ("id" INTEGER NOT NULL,
@@ -14,8 +14,9 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS "links"
 ("id" INTEGER NOT NULL,
 "long" TEXT NOT NULL,
 "short" TEXT NOT NULL,
+"count" INT NOT NULL,
 "access_id" INTEGER NOT NULL,
-"owner_id" INTEGER NOT NULL,
+"owner_id" INTEGER,
 primary key ("id" AUTOINCREMENT)
 );''')
 connect.commit()
@@ -29,20 +30,20 @@ primary key ("id" AUTOINCREMENT)
 connect.commit()
 
 #регистрация
-def registr(login,password):
+def registration(login,password):
     cursor.execute('''INSERT INTO
         users (login,password)
         VALUES (?,?) 
         ''', (login,password,))
     connect.commit()
 
-def searchUser(login):
+def take_user(login):
     return cursor.execute('''SELECT password 
         FROM users
         WHERE login = ? 
-        ''', (login,)).fetchone()
+        ''', (login,)).fetchall()
 
-def searchUserId(login):
+def take_userId(login):
     return cursor.execute('''SELECT id 
         FROM users
         WHERE login = ? 
@@ -56,18 +57,18 @@ def auth(login,password):
         ''', (login, password,)).fetchone()
     return "Вы вошли"
 
-def addLink(long,short,access_id,owner_id):
+def add_link(long,short,access_id,owner_id,count = 0):
     cursor.execute('''INSERT INTO
-        links (long,short,access_id,owner_id)
-        VALUES (?,?,?,?)
-        ''', (long,short,access_id,owner_id))
+        links (long,short,count,access_id,owner_id)
+        VALUES (?,?,?,?,?)
+        ''', (long,short,count,access_id,owner_id))
     connect.commit()
 
-def seacrAccesses():
+def take_accesses():
     return cursor.execute('''SELECT * FROM accesses
     ''',()).fetchall()
 
-def addAccesses(level_eng,level_ru):
+def add_accesses(level_eng,level_ru):
     cursor.execute('''INSERT INTO
         accesses (level_eng,level_ru)
         VALUES (?,?)
@@ -75,9 +76,52 @@ def addAccesses(level_eng,level_ru):
     connect.commit()
     return "Добавление категории прошло успешно"
 
-def searchUserLinks(user_id):
-    return cursor.execute('''SELECT links.long, links.short, accesses.level_ru
+def take_user_links(user_id):
+    return cursor.execute('''SELECT links.long, links.short, links.count, accesses.level_ru
     FROM links
     INNER JOIN accesses ON accesses.id = links.access_id
     WHERE links.owner_id = ?
     ''', (user_id,)).fetchall()
+
+def take_pseudonym(pseudonym):
+    return cursor.execute('''SELECT short 
+    FROM links
+    WHERE short = ?''',(pseudonym,)).fetchall()
+
+def take_long_user(long_link, user_id):
+    return cursor.execute('''SELECT long 
+    FROM links
+    WHERE long = ? AND owner_id = ?''',(long_link,user_id)).fetchall()
+
+def take_info_link(user_id, long_link):
+    return cursor.execute('''SELECT long, short, access_id
+    FROM links 
+    WHERE owner_id = ? AND long = ?''',(user_id,long_link)).fetchall()
+
+def update_link(long,short,access_id,user_id):
+    cursor.execute('''UPDATE links
+    SET short = ?, access_id = ?
+    WHERE owner_id = ? AND long = ?''',(short,access_id,user_id,long))
+    connect.commit()
+
+def delete_link(long_link, user_id):
+    cursor.execute('''DELETE
+    FROM links
+    WHERE long = ? AND owner_id = ?''',(long_link,user_id))
+    connect.commit()
+    return "Удалил"
+
+def take_link_info(short):
+    return cursor.execute('''SELECT long, count, access_id, owner_id
+    FROM links
+    WHERE short = ?''',(short,)).fetchall()
+
+def update_count(long,count):
+    cursor.execute('''UPDATE links
+    SET count = ?
+    WHERE long = ?''',(count,long))
+    connect.commit()
+
+def accesses_info():
+    return cursor.execute('''SELECT level_ru
+    FROM accesses''').fetchall()
--- main.py
+++ main.py
@@ -7,26 +7,51 @@ app = Flask(__name__, template_folder='templates', static_folder='templates/stat
 app.secret_key = os.urandom(30).hex()
 app.config['JWT_SECRET_KEY'] = 'super-secret'
 
-if len(seacrAccesses()) == 0:
+if len(take_accesses()) == 0:
     i = 0
-    accesses = [["public","публичный"],["private","приватный"],["general","общий"]]
+    accesses = [["public","публичный"],["general","общий"],["private","приватный"]]
     while i < len(accesses):
-        addAccesses(accesses[i][0],accesses[i][1])
+        add_accesses(accesses[i][0],accesses[i][1])
         i = i + 1
 
 @app.route('/', methods=['post', 'get'])
 def index():
     long_link = request.form.get('long_link')
     accesses = request.form.get('accesses')
+    pseudonym = request.form.get('pseudonym')
     massage = ''
-    if long_link != None:
+    short_link = ''
+    hosthref = request.host_url
+    if long_link != None and long_link != "":
         if 'user' in session:
-            user_short_link = ""
-            user_short_link = hashlib.md5(long_link.encode()).hexdigest()[:random.randint(8, 12)]
-            addLink(long_link,user_short_link,accesses,session['user'])
+            userLongLink = take_long_user(long_link, session['user'])
+            if len(userLongLink) == 0:
+                if pseudonym:
+                    checkPseudonym = take_pseudonym(pseudonym)
+                    if len(checkPseudonym) == 0:
+                        short_link = hosthref + "host/" + pseudonym
+                        add_link(long_link, pseudonym, accesses, session['user'])
+                    else:
+                        massage = 'Данный псевдоним занят'
+                else:
+                    user_short_link = hashlib.md5(long_link.encode()).hexdigest()[:random.randint(8, 12)]
+                    short_link = hosthref + "host/" + user_short_link
+                    add_link(long_link,user_short_link,accesses,session['user'])
+            else:
+                massage = 'Вы уже сокращали эту ссылку'
         else:
-            massage = 'войдите, чтобы использовать сокращатель'
-    return render_template("index.html", massage=massage)
+            if pseudonym:
+                checkPseudonym = take_pseudonym(pseudonym)
+                if len(checkPseudonym) == 0:
+                    short_link = hosthref + "host/" + pseudonym
+                    add_link(long_link, pseudonym, accesses, None)
+                else:
+                    massage = 'Данный псевдоним занят'
+            else:
+                user_short_link = hashlib.md5(long_link.encode()).hexdigest()[:random.randint(8, 12)]
+                short_link = hosthref + "host/" + user_short_link
+                add_link(long_link, user_short_link, accesses, None)
+    return render_template("index.html", massage=massage, short_link=short_link)
 
 @app.route('/auth', methods=['post', 'get'])
 def auth():
@@ -34,20 +59,19 @@ def auth():
     if request.method == 'POST':
         login = request.form.get('login')
         password = request.form.get('password')
-        user = searchUser(login)
         if login != "" and password != "":
-            if len(user) != 0:
-                if check_password_hash(user[0], password) == True:
-                    massage = 'вы успешно вошли'
-                    auth_user = searchUserId(login)[0]
+            user = take_user(login)
+            if len(user) > 0:
+                if check_password_hash(user[0][0], password) == True:
+                    auth_user = take_userId(login)[0]
                     session['user'] = auth_user
                     return redirect('/profile')
                 else:
-                    massage = 'неверный пароль'
+                    massage = 'Неверный пароль'
             else:
-                massage = 'такого пользователя нет'
+                massage = 'Такой пользователя нет'
         else:
-            massage = 'ваш логин или пароль неправильный'
+            massage = 'Неверный логин или пароль'
 
     return render_template("auth.html", massage=massage)
 
@@ -57,26 +81,91 @@ def reg():
     if request.method == 'POST':
         login = request.form.get('login')
         password = request.form.get('password')
-        confirmPassword = request.form.get('confirm_password')
-        user = searchUser(login)
+        user = take_user(login)
         if login != "" and password != "":
-            if user == None:
-                if confirmPassword == password:
-                    hash_password = generate_password_hash(password)
-                    registr(login, hash_password)
-                else:
-                    massage = 'пароли не совпадают'
+            if len(user) == 0:
+                hash_password = generate_password_hash(password)
+                registration(login, hash_password)
+                auth_user = take_userId(login)[0]
+                session['user'] = auth_user
+                return redirect('/profile')
             else:
-                massage = 'такой пользователь уже есть'
+                massage = 'Такой пользователь уже зарегестрирован'
         else:
-            massage = 'ваш логин или пароль неправильный'
+            massage = 'Неверный логин или пароль'
 
     return render_template("registr.html", massage=massage)
 
 @app.route('/profile', methods=['post', 'get'])
 def profile():
-    userlinks = searchUserLinks(session['user'])
-    return render_template("/profile.html", userlinks=userlinks)
+    btn_edit = request.form.get('btn_edit')
+    btn_delete = request.form.get('btn_delete')
+    links = take_user_links(session['user'])
+    hosthref = request.host_url
+    massage = ""
+
+    if btn_edit:
+        long = request.form.get('long')
+        short = request.form.get('short')
+        access = request.form.get('access')
+        if long != None and short != None:
+            if links[0][2] != access and take_pseudonym(short) == 0:
+                update_link(long, short, access, session['user'])
+                return redirect('/profile')
+            else:
+                if len(take_pseudonym(short)) > 0:
+                    massage = 'Данный псевдоним занят'
+                else:
+                    update_link(long, short, access, session['user'])
+                    return redirect('/profile')
+
+    if btn_delete:
+        delete_link(btn_delete, session['user'])
+        return redirect('/profile')
+
+    return render_template("/profile.html", links=links, hosthref=hosthref, massage=massage)
+
+@app.route('/logout', methods=['post', 'get'])
+def logout():
+    session.clear()
+    return redirect('/')
+
+@app.route('/report')
+def message():
+    return render_template('/report.html')
+
+@app.route('/404')
+def error():
+    return render_template('/404.html')
+
+@app.route('/host/<shortlink>')
+def link(shortlink):
+    if len(take_link_info(shortlink)) != 0:
+        user_link = take_link_info(shortlink)[0]
+        print(user_link)
+        count = user_link[1]
+        if user_link[2] == 1:
+            update_count(user_link[0],count + 1)
+            return redirect(user_link[0])
+        elif user_link[2] == 2:
+            if 'user' in session:
+                update_count(user_link[0], count + 1)
+                return redirect(user_link[0])
+            else:
+                return redirect('/auth')
+        elif user_link[2] == 3:
+            if 'user' in session:
+                if len(take_long_user(user_link[0],session['user'])) > 0:
+                    update_count(user_link[0], count + 1)
+                    return redirect(user_link[0])
+                else:
+                    return redirect('/report')
+            else:
+                return redirect('/report')
+        elif user_link[2] == None:
+            return redirect('/report')
+    else:
+        return redirect('/404')
 
 if __name__ == '__main__':
     app.run()
